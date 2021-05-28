c8ca347010e94a3b6f6b8b85a671ec5399fcdc49
svdegron <svdegron@gmail.com>
2021-05-28 17:08:37 +0500

    Added methods so as not to forget that they still here: 	- toString 	- equals 	- hasCode 	- it is fast forget when you write a little

--- src/main/java/ru/parsentev/task_002/Point.java
+++ src/main/java/ru/parsentev/task_002/Point.java
@@ -11,7 +11,7 @@ package ru.parsentev.task_002;
  * @since 19.07.2016
  */
 public class Point {
-	
+
 //	private static final Logger log = getLogger(Point.class);
 
 	private final int x;
@@ -23,17 +23,64 @@ public class Point {
 	}
 
 	public double distanceTo(final Point point) {
-		
+
 		// If I remember distance between two point calculated by formula
 		// sqrt ( (x1^2 + x2^2) - (y1^2 + y2^2) )
 		// now, let's ask google
 		// I wrong, formula is
 		// sqrt ( (x1 - x2)^2 + (y1 - y2)^2 )
-		
-		double distance = Math.sqrt( (this.x - point.x) * (this.x - point.x) + (this.y - point.y) * (this.y - point.y) );
+
+		double distance = Math.sqrt((this.x - point.x) * (this.x - point.x) + (this.y - point.y) * (this.y - point.y));
 		// or
-		// double distance = Math.sqrt( Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2) );
+		// double distance = Math.sqrt( Math.pow(this.x - point.x, 2) + Math.pow(this.y
+		// - point.y, 2) );
 		
+		// at solution added function «abs»
+
 		return distance;
 	}
+
+	// I know it was very simple to be true → google → I knew it was too easy to be
+	// true
+	// need toString / equals and hash code, how I could forget
+	// mouse right button → Source…
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+		// at solution: return String.format("(%s, %s)", this.x, this.y);
+		// it just format. Maybe looks better… I don't want repeat «solution»
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+
+		// and this I think need changes. But I not know why… yet
+		// int result = x;
+		// result = 31 * result + y;
+		// return result;
+	}
+
+	// at solution more compact. Return true or false in one line with if
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
 }

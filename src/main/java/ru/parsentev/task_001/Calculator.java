877e843c0831361d017dda38d490b79ded7d3180
svdegron <svdegron@gmail.com>
2021-05-28 16:00:30 +0500

    Done 	- at first, not guessed how return result. And watched done solution 	- I am a stupid. Forgot how throw exception. How correct write I remember, but it take me more time than I hope 	- make two way for expand. First - library Math, second - without «Math» (by loop)

--- src/main/java/ru/parsentev/task_001/Calculator.java
+++ src/main/java/ru/parsentev/task_001/Calculator.java
@@ -10,29 +10,46 @@ import static org.slf4j.LoggerFactory.getLogger;
  * @since 18.07.2016
  */
 public class Calculator {
-    private static final Logger log = getLogger(Calculator.class);
 
-    public double getResult() {
-        throw new UnsupportedOperationException();
-    }
+	private static final Logger log = getLogger(Calculator.class);
+	private double result = 0; // I did not guessed. Looked at a ready-made solution
 
-    public void add(double first, double second) {
-        throw new UnsupportedOperationException();
-    }
+	public double getResult() {
+		return this.result;
+	}
 
-    public void substract(double first, double second) {
-        throw new UnsupportedOperationException();
-    }
+	public void add(double first, double second) {
+		this.result = first + second;
+	}
 
-    public void multiple(double first, double second) {
-        throw new UnsupportedOperationException();
-    }
+	public void substract(double first, double second) {
+		this.result = first - second;
+	}
 
-    public void div(double first, double second) {
-        throw new UnsupportedOperationException();
-    }
+	public void multiple(double first, double second) {
+		this.result = first * second;
+	}
 
-    public void expand(double first, double second) {
-        throw new UnsupportedOperationException();
-    }
+	public void div(double first, double second) {
+
+		if (second == 0) {
+			throw new IllegalStateException();
+		}
+
+		this.result = first / second;
+
+	}
+
+	public void expand(double first, double second) {
+		
+		this.result = Math.pow(first, second);
+		
+		// my way, for example. It is working too
+		// this.result = 1;
+		// for (int i = 0; i <= second; i++) {
+		// 	this.result = this.result * first;
+		// }
+		// now, need watch solution branch ☺
+		
+	}
 }
